ea0e98207c2a50de0581350608f475ad7f7a7502
Jin Kun Chai <jk.chai@hostbridge.com>
2022-01-25 12:43:59 -0600

    Update child and parent components for advanced props tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,56 @@
 import './App.css';
 import { Greet } from './components/Greet';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
+// import { Person } from './components/Person';
+// import { PersonList } from './components/PersonList';
+import { Heading } from './components/Heading';
+import { Status } from './components/Status';
+import { Oscar } from './components/Oscar';
 
+
+// Advanced Props: Union,
 function App() {
-    const fullname = {
-        first: 'Mike',
-        last: 'Lord'
-    }
-
-    const personList = [
-        {
-            first:'dear',
-            last:'lies'
-        },
-        {
-            first: 'hello',
-            last: "world"
-        },
-        {
-            first: "homie",
-            last: 'god'
-        }
-    ]
-
-  return (
-    <div className="App">
-        <Greet value="This is it!!" count={5} condition={false}/>
-        <Person name={fullname}/>
-        <PersonList names={personList}/>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Status status='loading' />
+            <Heading>Placeholder text</Heading>
+            <Oscar>
+                <Heading>Oscar goes to Leornado Dicarpio!!</Heading>
+            </Oscar>
+            <Greet value='Vishwas' condition={ false } />
+        </div>
+    )
 }
+
+
+//// Greet | Person | PersonList comps examples
+// function App() {
+//     const fullname = {
+//         first: 'Mike',
+//         last: 'Lord'
+//     }
+
+//     const personList = [
+//         {
+//             first:'dear',
+//             last:'lies'
+//         },
+//         {
+//             first: 'hello',
+//             last: "world"
+//         },
+//         {
+//             first: "homie",
+//             last: 'god'
+//         }
+//     ]
+
+//   return (
+//     <div className="App">
+//         <Greet value="This is it!!" count={5} condition={false}/>
+//         <Person name={fullname}/>
+//         <PersonList names={personList}/>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,17 +1,18 @@
 // example of basic types
-
+// with advanced props using optional operator ?
 type GreetProps = {
     value: string;
-    count: number;
+    count?: number; // ? indicates optional
     condition: boolean;
 };
 
 export const Greet = (props: GreetProps) => {
+    const { count = 0 } = props; // assign 0 if optional count is null/undefined/false
     return (
         <div>
             <h2>
                 {props.condition 
-                ? `Something Happenning.... ${props.value} ${props.count} Trophies!!`
+                ? `Something Happenning.... ${props.value} ${count} Trophies!!`
                 : "OH NO!! No Trophies..."
                 }
             </h2>
